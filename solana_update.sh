3c0c4e165d2461b1727e751f461e1fba224e8d7e
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2021-05-06 00:26:33 +0300

    Update solana_update.sh

--- solana_update.sh
+++ solana_update.sh
@@ -1,27 +1,19 @@
 # Taken from https://www.youtube.com/watch?v=vzozlM-5Hq8
 
-solana --version
-#solana-cli 1.6.4
-
-solana-install update
-# Update successful to 1.6.6
-
-solana slot
+`solana --version && solana slot && solana-install update`
+# 1.6.7
 # 71876724
+# installed
 
-# try to find free space to reload service while not generating block by or node - 4P8
-solana leader-schedule -u localhost | grep 4P8 | grep 7187 
-# 7187 = first 4 counts of block #
+# try to find free space to reload service while not generating block
+`solana leader-schedule -u localhost | grep 4P8 | grep $(solana slot | cut -c 1-4)`
 
-# look through snapshot close to current slot # 
-ls -la /root/solana/ledger/snapshot*tar*
+# look through snapshot close to current slot #
+#ls -la /root/solana/ledger/snapshot*tar*
 
 #-rw-r--r-- 1 root root  179951193 Apr 10 10:52 snapshot-71030051-8DZKXZNi9TS4uqVpWwbqHRZczDcZCxcJaWpVz5FSDW6W.tar.zst
 #-rw-r--r-- 1 root root 2168606720 Apr 14 21:14 snapshot-71876032-9hgot9X1KJJ1C7Es2PFW3sf7zS8oQPgzzqmzcWcxL4Q.tar
 #-rw-r--r-- 1 root root 2167183360 Apr 14 21:18 snapshot-71876588-2ScbsPKBorFvpNm4Jn5R264oWuaPmhNZGFEn52KrYkdJ.tar
 
-systemctl restart solana
-
-solana catchup /root/solana/validator-keypair.json --our-localhost
-
-solana slot
+When you have time - install and wait for catchup
+`solana-install update && systemctl restart solana && sleep 30 && solana catchup /root/solana/validator-keypair.json --our-localhost`
