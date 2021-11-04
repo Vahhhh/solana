77f32894d489f3d063bb7c6c08fb001fd32f9ee3
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2021-11-04 16:03:43 +0300

    Update performance.sh

--- performance.sh
+++ performance.sh
@@ -1,1 +1,5 @@
-sudo apt-get install cpufrequtils -y && echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils && sudo systemctl disable ondemand && sudo systemctl restart cpufrequtils.service
+#!/bin/bash
+sudo apt-get install cpufrequtils -y && \
+echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils && \
+sudo systemctl disable ondemand && \
+sudo systemctl restart cpufrequtils.service
