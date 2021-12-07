7171f333b85c050764b66f431dd616cbff10c999
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2021-12-07 23:53:02 +0300

    Update monitoring_config.py

--- monitoring/monitoring_config.py
+++ monitoring/monitoring_config.py
@@ -1,10 +1,1 @@
-from common import ValidatorConfig
 
-config = ValidatorConfig(
-    validator_name="sol02-stakeiteasy-testnet",
-    secrets_path="/root/solana",
-    local_rpc_address="http://localhost:8899",
-    remote_rpc_address="https://api.testnet.solana.com",
-    cluster_environment="testnet",
-    debug_mode=False
-)
