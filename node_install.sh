9b60470045eeabcc6e16ffd2d1b98c6e9ab84a2b
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2022-01-20 21:10:48 +0300

    Update node_install.sh
    added fail2ban for sshd, changed solana version

--- node_install.sh
+++ node_install.sh
@@ -337,6 +337,26 @@ printf '##INPUTS
 systemctl restart telegraf
 
 
+# install fail2ban
+apt -y install fail2ban iptables && \
+printf '[DEFAULT]
+ignoreip = 93.174.52.0/23
+bantime  = 21600
+findtime  = 300
+maxretry = 3
+banaction = iptables-multiport
+backend = systemd
+
+[sshd]
+enabled = true
+' > /etc/fail2ban/jail.local && \
+systemctl enable fail2ban && systemctl restart fail2ban
+
+iptables -nvL
+fail2ban-client status sshd
+
+
+
 # configure fw
 sudo ufw reset
 
