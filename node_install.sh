79fe4ac203a29c37e457feb18357622cbf24e90f
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2022-03-14 10:34:38 +0300

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -327,14 +327,13 @@ bantime  = 21600
 findtime  = 300
 maxretry = 3
 banaction = iptables-multiport
-backend = systemd
-
+backend = auto
 [sshd]
 enabled = true
 ' > /etc/fail2ban/jail.local && \
-systemctl enable fail2ban && systemctl restart fail2ban
-
-iptables -nvL
+systemctl enable fail2ban && systemctl restart fail2ban && \
+sleep 1 && \
+iptables -nvL && \
 fail2ban-client status sshd
 
 
