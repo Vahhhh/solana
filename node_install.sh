04675be933b712a9bb38feb8a9e86628f45e79cf
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2022-03-30 22:28:27 +0300

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -78,7 +78,8 @@ solana config set --keypair ~/solana/validator-keypair.json
 
 solana-keygen new -o ~/solana/vote-account-keypair.json
 
-solana create-vote-account -v --authorized-withdrawer ~/solana/validator-keypair.json --commission 100 -k ~/solana/validator-keypair.json ~/solana/vote-account-keypair.json ~/solana/validator-keypair.json
+solana create-vote-account -v --commission 100 -k ~/solana/validator-keypair.json \
+~/solana/vote-account-keypair.json ~/solana/validator-keypair.json ~/solana/validator-keypair(withdrawer).json
 
 printf '[Unit]
 Description=Solana TdS node
