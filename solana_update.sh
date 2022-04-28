ed083427b379caa2e965564563b78763cef1f35f
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2022-04-28 11:43:17 +0300

    Update solana_update.sh

--- solana_update.sh
+++ solana_update.sh
@@ -1,13 +1,13 @@
 # Taken from https://www.youtube.com/watch?v=vzozlM-5Hq8
 
-solana --version && solana slot && solana-install update
+#solana --version && solana slot && solana-install update
 # 1.6.7
 # 71876724
 # installed
 
 # try to find free space to reload service while not generating block
 # need file see-schedule.sh in the same directory
-./see-schedule.sh | grep -m1 -A11 "new>" | sed -n -e 1p -e 5p -e 9p
+#./see-schedule.sh | grep -m1 -A11 "new>" | sed -n -e 1p -e 5p -e 9p
 
 # look through snapshot close to current slot #
 #ls -la /root/solana/ledger/snapshot*tar*
@@ -17,4 +17,6 @@ solana --version && solana slot && solana-install update
 #-rw-r--r-- 1 root root 2167183360 Apr 14 21:18 snapshot-71876588-2ScbsPKBorFvpNm4Jn5R264oWuaPmhNZGFEn52KrYkdJ.tar
 
 #When you have time - install and wait for catchup
-solana-install update && systemctl restart solana && sleep 30 && solana catchup /root/solana/validator-keypair.json --our-localhost
+#solana-install update && systemctl restart solana && sleep 30 && solana catchup /root/solana/validator-keypair.json --our-localhost
+
+apt install -y screen && screen -AdmS solana /bin/bash -c 'solana-install init 1.10.10 && solana-validator --ledger /root/solana/ledger/ wait-for-restart-window --max-delinquent-stake 10 && systemctl restart solana && err=1; while [ $err -eq 1 ]; do solana catchup /root/solana/validator-keypair.json --our-localhost; err=$?; sleep 5; done'
