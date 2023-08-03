121644cfff7558a092ff9021ee9aec2a772c4a60
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-08-03 16:48:44 +0300

    Update solana_node_install.sh

--- solana_node_install.sh
+++ solana_node_install.sh
@@ -154,7 +154,7 @@ StartLimitIntervalSec=0
 Type=simple
 Restart=always
 RestartSec=1
-LimitNOFILE=1024000
+LimitNOFILE=1000000
 Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=tds,u=testnet_write,p=c4fa841aa918bf8274e3e2a44d77568d9861b3ea"
 ExecStart=/root/.local/share/solana/install/active_release/bin/solana-validator \
 --entrypoint entrypoint3.testnet.solana.com:8001 \
