eb1bcb552dacf333e345abc7a74441ff610b7e26
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-08-14 22:41:16 +0300

    Create limits.sh

---
+++ limits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+bash -c "cat >/etc/sysctl.d/21-solana-validator.conf <<EOF
+# Increase UDP buffer sizes
+net.core.rmem_default = 134217728
+net.core.rmem_max = 134217728
+net.core.wmem_default = 134217728
+net.core.wmem_max = 134217728
+
+# Increase memory mapped files limit
+vm.max_map_count = 1000000
+
+# Increase number of allowed open file descriptors
+fs.nr_open = 1000000
+EOF"
+
+sysctl -p /etc/sysctl.d/21-solana-validator.conf
+
+bash -c "cat >/etc/security/limits.d/90-solana-nofiles.conf <<EOF
+# Increase process file descriptor count limit
+* - nofile 1000000
+EOF"
