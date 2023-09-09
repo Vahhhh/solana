733233cd40bd1d3dff74d964cd422cc742dc055a
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-09-09 18:12:53 +0300

    Update linux_new.sh

--- linux_new.sh
+++ linux_new.sh
@@ -2,3 +2,5 @@
 
 export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r;"
 export HISTTIMEFORMAT='%F %T '
+
+sudo systemctl stop snapd && sudo systemctl disable snapd && sudo apt purge snapd -y && rm -rf ~/snap && sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd /root/snap
