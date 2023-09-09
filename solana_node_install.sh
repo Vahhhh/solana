c7e7f48927c6771f2aeac0fc20be94ec4b704a1e
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-09-09 17:59:10 +0300

    Update solana_node_install.sh

--- solana_node_install.sh
+++ solana_node_install.sh
@@ -12,8 +12,8 @@ NETWORK=testnet
 SOLANA_PATH="/root/solana"
 IDENTITY_PATH="/root/solana/validator-keypair.json"
 VOTE_PATH="/root/solana/vote-account-keypair.json"
-VER_MAINNET=1.14.23
-VER_TESTNET=1.14.24
+VER_MAINNET=1.14.27
+VER_TESTNET=1.16.12
 SWAP_PATH="/swapfile"
 
 # Input variables
