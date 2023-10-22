907d98461d483cd56d485a722ce64b444f11a031
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-10-22 08:41:37 +0300

    Update limits.sh

--- limits.sh
+++ limits.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # wget -O - https://raw.githubusercontent.com/Vahhhh/solana/main/limits.sh | bash
 
+bash -c "echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+
 bash -c "cat >/etc/sysctl.d/21-solana-validator.conf <<EOF
 # Increase UDP buffer sizes
 net.core.rmem_default = 134217728
@@ -16,6 +18,7 @@ fs.nr_open = 2048000
 EOF"
 
 sysctl -p /etc/sysctl.d/21-solana-validator.conf
+sysctl -p
 
 bash -c "cat >/etc/security/limits.d/90-solana-nofiles.conf <<EOF
 # Increase process file descriptor count limit
