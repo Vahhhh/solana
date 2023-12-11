d98de69906c3899f72077f51b725695ac7525f47
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-12-12 00:14:22 +0300

    Update solana_node_install.sh

--- solana_node_install.sh
+++ solana_node_install.sh
@@ -71,6 +71,53 @@ read -r -s VOTE_DATA
 echo $VOTE_DATA > $VOTE_PATH
 fi
 
+printf "${C_LGn}Enter the swap size [$SWAPSIZE]:${RES} "
+read -r SWAPSIZE_INPUT
+if [ -n "$SWAPSIZE_INPUT" ]; then
+SWAPSIZE=$SWAPSIZE_INPUT
+fi
+
+printf "${C_LGn}Enter the ramdisk size [$SWAPSIZE2]:${RES} "
+read -r SWAPSIZE2_INPUT
+if [ -n "$SWAPSIZE2_INPUT" ]; then
+SWAPSIZE2=$SWAPSIZE2_INPUT
+fi
+
+cat > /root/solana/solana.logrotate <<EOF
+/root/solana/solana.log {
+  rotate 7
+  daily
+  missingok
+  postrotate
+    systemctl kill -s USR1 solana.service
+  endscript
+}
+EOF
+
+#if [ ! -f $SWAP_PATH ]; then
+    swapoff -a
+    dd if=/dev/zero of=$SWAP_PATH bs=1G count=$SWAPSIZE
+    chmod 600 $SWAP_PATH
+    mkswap $SWAP_PATH
+    swapon $SWAP_PATH
+
+    # delete other swaps from /etc/fstab
+    sed -e '/swap/s/^/#\ /' -i_backup /etc/fstab
+
+    ## add to /etc/fstab
+    echo $SWAP_PATH ' none swap sw 0 0' >> /etc/fstab
+#fi
+
+if [ ! -d "/mnt/ramdisk" ]; then
+    # ramdisk
+    ## add to /etc/fstab
+    echo 'tmpfs /mnt/ramdisk tmpfs nodev,nosuid,noexec,nodiratime,size='$SWAPSIZE2'G 0 0' >> /etc/fstab
+
+    mkdir -p /mnt/ramdisk
+    mount /mnt/ramdisk
+fi
+
+
 echo ""
 
 #: ${value2:=$default1}
@@ -82,6 +129,7 @@ curl -sL https://repos.influxdata.com/influxdata-archive_compat.key | apt-key ad
 echo "deb https://repos.influxdata.com/ubuntu bionic stable" >> /etc/apt/sources.list.d/influxdata.list && \
 apt-get update -y && apt-get upgrade -y && apt-get -y install git telegraf jq bc screen python3-pip && systemctl stop telegraf && pip3 install numpy requests
 
+if [ "$CLIENT" == "solana" ]; then
 cd /root/solana
 
 sh -c "$(curl -sSfL https://release.solana.com/v$SOLANAVERSION/install)" && \
@@ -185,54 +233,90 @@ WantedBy=multi-user.target
 ' > /root/solana/solana.service
 fi
 
-#if [ "$SOLANAVERSION" == "1.16.17" ]; then
-#cp /root/solana/solana.service /root/solana/solana.service.$(date '+%Y-%m-%d') && curl https://raw.githubusercontent.com/Vahhhh/solana/main/config_main/solana.service.1.16.17 > /root/solana/solana.service
-#fi
+elif [ "$CLIENT" == "jito" ]; then
 
-printf "${C_LGn}Enter the swap size [$SWAPSIZE]:${RES} "
-read -r SWAPSIZE_INPUT
-if [ -n "$SWAPSIZE_INPUT" ]; then
-SWAPSIZE=$SWAPSIZE_INPUT
+TAG=v$SOLANAVERSION-jito
+cd && curl https://sh.rustup.rs -sSf | sh && source $HOME/.cargo/env && \
+rustup component add rustfmt && rustup update && \
+apt-get install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler && \
+
+git clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules && \
+cd jito-solana && \
+git checkout tags/$TAG && \
+git submodule update --init --recursive && \
+CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
+
+ln -s /root/.local/share/solana/install/releases/"$TAG" /root/.local/share/solana/install/active_release
+export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
+echo 'export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"' >> /root/.bashrc
+
+VOTE_ACCOUNT_ADDRESS=$(solana address -k $VOTE_PATH)
+
+if [ "$NETWORK" == "mainnet-beta" ]; then
+SWAPSIZE=300
+SWAPSIZE2=250
+printf '[Unit]
+Description=Solana Mainnet node
+After=network.target syslog.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+LimitNOFILE=2048000
+Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password"
+ExecStart=/root/.local/share/solana/install/active_release/bin/solana-validator \
+--no-skip-initial-accounts-db-clean \
+--identity /root/solana/validator-keypair.json \
+--vote-account $VOTE_ACCOUNT_ADDRESS \
+--authorized-voter /root/solana/validator-keypair.json \
+--rpc-port 8899 \
+--entrypoint 184.105.146.35:8000 \
+--entrypoint se1.laine.co.za:8001 \
+--entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+--log /root/solana/solana.log \
+--ledger /root/solana/ledger \
+--accounts /root/solana/accounts \
+--dynamic-port-range 8001-8050 \
+--no-port-check \
+--private-rpc \
+--rpc-bind-address 127.0.0.1 \
+--tower /root/solana/ledger \
+--snapshots /root/solana/snapshots \
+--no-check-vote-account \
+--expected-shred-version 56177 \
+--known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+--known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+--known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+--known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+--only-known-rpc \
+--limit-ledger-size \
+--wal-recovery-mode skip_any_corrupted_record \
+--incremental-snapshots \
+--replay-slots-concurrently \
+--contact-debug-interval 1000000 \
+--minimal-snapshot-download-speed 30000000 \
+--rocksdb-shred-compaction fifo \
+--tip-payment-program-pubkey T1pyyaTNZsKv2WcRAB8oVnk93mLJw2XzjtVYqCsaHqt \
+--tip-distribution-program-pubkey 4R3gSG8BpU4t19KYj8CfnbtRpnT8gtk4dvTHxVRwc2r7 \
+--merkle-root-upload-authority GZctHpWXmsZC1YHACTGGcHhYxjdRqQvTpYkb9LMvxDib \
+--commission-bps 800 \
+--relayer-url http://frankfurt.mainnet.relayer.jito.wtf:8100 \
+--block-engine-url https://frankfurt.mainnet.block-engine.jito.wtf \
+--shred-receiver-address 145.40.93.84:1002
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+[Install]
+WantedBy=multi-user.target
+' > /root/solana/solana.service
+
+elif [ "$NETWORK" == "testnet" ]; then
+
 fi
 
-printf "${C_LGn}Enter the ramdisk size [$SWAPSIZE2]:${RES} "
-read -r SWAPSIZE2_INPUT
-if [ -n "$SWAPSIZE2_INPUT" ]; then
-SWAPSIZE2=$SWAPSIZE2_INPUT
-fi
 
-cat > /root/solana/solana.logrotate <<EOF
-/root/solana/solana.log {
-  rotate 7
-  daily
-  missingok
-  postrotate
-    systemctl kill -s USR1 solana.service
-  endscript
-}
-EOF
-
-#if [ ! -f $SWAP_PATH ]; then
-    swapoff -a
-    dd if=/dev/zero of=$SWAP_PATH bs=1G count=$SWAPSIZE
-    chmod 600 $SWAP_PATH
-    mkswap $SWAP_PATH
-    swapon $SWAP_PATH
-
-    # delete other swaps from /etc/fstab
-    sed -e '/swap/s/^/#\ /' -i_backup /etc/fstab
-
-    ## add to /etc/fstab
-    echo $SWAP_PATH ' none swap sw 0 0' >> /etc/fstab
-#fi
-
-if [ ! -d "/mnt/ramdisk" ]; then
-    # ramdisk
-    ## add to /etc/fstab
-    echo 'tmpfs /mnt/ramdisk tmpfs nodev,nosuid,noexec,nodiratime,size='$SWAPSIZE2'G 0 0' >> /etc/fstab
-
-    mkdir -p /mnt/ramdisk
-    mount /mnt/ramdisk
 fi
 
 if [ ! -f "/etc/systemd/system/solana.service" ]; then
@@ -250,6 +334,7 @@ systemctl restart logrotate.service
 systemctl enable solana.service
 systemctl start solana.service
 
+
 adduser telegraf sudo && \
 adduser telegraf adm && \
 echo "telegraf ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
@@ -259,6 +344,10 @@ rm -rf /etc/telegraf/telegraf.conf && \
 cd /root/solana && git clone https://github.com/stakeconomy/solanamonitoring/ && \
 mkdir -p /root/tmp_git && cd $_ && git clone https://github.com/Vahhhh/solana/ && \
 cp -r /root/tmp_git/solana/monitoring /root/solana/ && chmod +x /root/solana/monitoring/output_starter.sh && cd /root/solana
+
+if [ "$CLIENT" == "jito" ]; then
+mv /root/solana/monitoring/solana_rpc_jito.py /root/solana/monitoring/solana_rpc.py
+fi
 
 printf 'from common import ValidatorConfig
 config = ValidatorConfig(
