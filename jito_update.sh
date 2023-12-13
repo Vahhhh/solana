1b02f00588f4852a0726c82e2f92d323375b0e3d
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-12-13 19:32:22 +0300

    Create jito_update.sh

---
+++ jito_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# wget -O - https://raw.githubusercontent.com/Vahhhh/solana/jito_update.sh | bash
+set -e -x -v
+
+SOLANAVERSION=1.16.23
+
+printf "${C_LGn}Enter the software version [$SOLANAVERSION]:${RES} "
+read -r SOLANAVERSION_INPUT
+if [ -n "$SOLANAVERSION_INPUT" ]; then
+SOLANAVERSION=$SOLANAVERSION_INPUT
+fi
+export TAG=v$SOLANAVERSION-jito
+
+cd ~/jito-solana && git pull ; git checkout tags/$TAG && git submodule update --init --recursive && \
+CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
+
+ln -nsf /root/.local/share/solana/install/releases/"$TAG" /root/.local/share/solana/install/active_release
