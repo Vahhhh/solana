c5aac635d56bcbca485c8e99d6cdc5cdf01e4864
Andrey Vakhutinskiy <57785562+Vahhhh@users.noreply.github.com>
2023-12-29 00:34:20 +0300

    Update linux_performance.sh

--- linux_performance.sh
+++ linux_performance.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/Vahhhh/solana/main/linux_performance.sh | bash
 sudo apt-get install cpufrequtils -y && \
 echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils && \
 sudo systemctl disable ondemand && \
